ef25ef7bcdb5632879f1fb55511e7c5fbe7fac05
Júlia <168246513+diamondxorb@users.noreply.github.com>
2025-01-15 16:27:28 -0300

    Create Heap Binário.py

--- Heap Binário.py
+++ Heap Binário.py
@@ -7,7 +7,7 @@ class BinHeap:
         self.heapList.append(k)
         self.currentSize = self.currentSize+1
         self.percUp(self.currentSize)
-    
+
     def percUp(self, i):
         while i//2 > 0:
             if self.heapList[i] < self.heapList[i//2]:
@@ -15,7 +15,7 @@ class BinHeap:
                 self.heapList[i//2] = self.heapList[i]
                 self.heapList[i] = tmp
             i = i//2
-    
+
     def delMin(self):
         retval = self.heapList[1]
         self.heapList[1] = self.heapList[self.currentSize]
@@ -23,7 +23,7 @@ class BinHeap:
         self.heapList.pop()
         self.percDown(1)
         return retval
-    
+
     def percDown(self, i):
         while i*2 <= self.currentSize:
             mc = self.minChild(i)
@@ -32,7 +32,7 @@ class BinHeap:
                 self.heapList[i] = self.heapList[mc]
                 self.heapList[mc] = tmp
             i = mc
-    
+
     def minChild(self, i):
         if (i*2)+1 > self.currentSize:
             return i*2
@@ -41,7 +41,7 @@ class BinHeap:
                 return i*2
             else:
                 return (i*2)+1
-            
+
     def buildHeap(self, alist):
         i = len(alist)//2
         self.currentSize = len(alist)
